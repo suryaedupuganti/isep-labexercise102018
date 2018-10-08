fd92f28ceb54be4ae29296fd1df25b3b74165a26
Surya Vara Prasad E <edpugantisuryachowdary@gmail.com>
2018-10-08 14:48:30 +0300

    Exercise 1.2

---
+++ src/main/java/dblab/hello/App.java
@@ -0,0 +1,16 @@
+package dblab.hello;
+
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        if (args.length >0)
+        {
+          int i=0;
+          for (String argument : args)
+          {
+            System.out.format("argument [%d]: %s %n", i++, argument);
+          }
+        }
+    }
+}
